825c20c0158997b0a2e1810d87da640ce65d95bf
Naveed Solangi <naveeddsolangi@gmail.com>
2025-11-21 02:32:45 +0500

    final push

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -2610,7 +2610,7 @@ export default function CoursesPage() {
                       onClick={() => openModal(course)}
                     >
                       <CalendarCheck className="w-5 h-5 mr-2 transition-transform group-hover/btn:scale-110" />
-                      {t('bookMySeat') || 'Book My Seat'}
+                      {t('learnmore') || 'Book My Seat'}
                     </Button>
                   </CardContent>
                 </Card>
