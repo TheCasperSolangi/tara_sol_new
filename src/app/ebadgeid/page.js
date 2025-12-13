f687cd76a7aa6be2e0b5e15417f11d28b65e5e96
Naveed Solangi <naveeddsolangi@gmail.com>
2025-12-14 04:47:56 +0500

    ebadge introduced

---
+++ src/app/ebadgeid/page.js
@@ -0,0 +1,14 @@
+import AboutPage from "../../components/AboutUs";
+import AIServices from "../../components/AIVision";
+import CustomServices from "../../components/CustomService";
+import EBadgeIDService from "../../components/ebadgeid";
+
+
+export default function ebadgeid() {
+  return (
+    <>
+    <EBadgeIDService />
+  
+    </>
+  );
+}
